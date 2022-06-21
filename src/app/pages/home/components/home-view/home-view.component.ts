e0ede0afb0b579f8ec7ea35da64d80743c2fbeb7
valerian.ignat <valerian.ignat@fortech.ro>
2022-06-21 16:23:56 +0300

    fix

--- src/app/pages/home/components/home-view/home-view.component.ts
+++ src/app/pages/home/components/home-view/home-view.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-view',
   templateUrl: './home-view.component.html',
   styleUrls: ['./home-view.component.scss']
 })
-export class HomeViewComponent implements OnInit {
+export class HomeViewComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
-
 }
